0e603c9192d42c63bb42da7fe17e99bd0102ea67
Denis <last.head@yandex.ru>
2020-04-08 00:01:13 +0300

    App.js to functional component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React, {useEffect} from 'react';
 import {
   Switch,
-  withRouter
+  useHistory
 } from "react-router-dom";
 import Home from './pages/notes'
 import Login from './pages/login'
 import './App.scss';
 import Edit from "./pages/edit/edit";
-import { browserHistory } from 'react-router';
-class App extends Component {
-  render() {
-    const { history } = this.props.history;
-    return (
-      <div className="App app__wrapper">
-        <div className="progress-bar" />
+import { setUserData } from './store/actions';
+import {useDispatch, useSelector} from "react-redux";
+import PrivateRoute from "./components/PrivateRoute";
+import LoginRoute from "./components/LoginRoute";
+
+export default function App() {
+  const dispatch = useDispatch();
+  const session = useSelector( state => state.session);
+  const asyncGetUserData = async () => {
+    if (localStorage.getItem('refreshToken')) {
+      try {
+        dispatch(await setUserData());
+      } catch(error) {}
+    }
+  };
+  useEffect( () => { asyncGetUserData(); }, []);
+  const history = useHistory();
+  return (
+    <div className="App app__wrapper">
+      { session.isLoading &&  <div className="progress-bar hidden" /> }
+      { !session.isLoading &&
         <Switch>
-          <Route exact history={history} path='/' component={Home}/>
-          <Route exact history={history} path='/:mode/:id' component={Edit}/>
-          <Route exact history={history} path='/:mode' component={Edit}/>
-          <Route path='/login' history={history} component={Login}/>
+          <PrivateRoute isAuth={!!session.user} exact history={history} path='/' component={Home}/>
+          <LoginRoute isAuth={!!session.user} path='/login' history={history} component={Login}/>
+          <PrivateRoute isAuth={!!session.user} exact history={history} path='/:mode/:id' component={Edit}/>
+          <PrivateRoute isAuth={!!session.user} exact history={history} path='/:mode' component={Edit}/>
         </Switch>
-      </div>
-    );
-  }
+      }
+    </div>
+  );
 }
 
-export default withRouter(App);
