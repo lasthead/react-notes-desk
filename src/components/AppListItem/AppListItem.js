be1eb4c25f9d88ce6d83616cc32f3c5e82a94c8e
Denis <last.head@yandex.ru>
2020-04-23 14:15:33 +0300

    Refactoring date updater in AppListItem component

--- src/components/AppListItem/AppListItem.js
+++ src/components/AppListItem/AppListItem.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./AppListItem.scss";
 import dayjs from 'dayjs';
 import {timeSince} from "../../utils/baseHelper";
 
 export default function AppListItem(props) {
-  let timeRate = 1000;
-  if (Date.now() > dayjs(props.updated_at).valueOf() + 3600 * timeRate) {
-    timeRate = timeRate * 60 * 60;
-  };
+  const [noteDateAgo, setNoteDateAgo] = useState(
+    timeSince(new Date(dayjs(props.updated_at).valueOf()))
+  );
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setNoteDateAgo(
+        timeSince(new Date(dayjs(props.updated_at).valueOf()))
+      );
+    }, 1000);
 
-  const formatedDate = timeSince(new Date(dayjs(props.updated_at).valueOf() - timeRate));
+    return () => clearInterval(intervalId);
+  }, [props.updated_at]);
 
   return (
     <div onClick={ () => props.onClick(props.id) } className="item item__container">
       <div className="item__title">{ props.name }</div>
-      <div className="item__date"> {formatedDate} ago</div>
+      <div className="item__date"> {noteDateAgo} ago</div>
       <div className="item__text">
         { props.text }
       </div>
